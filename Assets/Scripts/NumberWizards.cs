5dda878116f6d084a88a404859fbf8f05fe1d723
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2020-08-11 18:56:30 +0300

    Wizard

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using TMPro;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour
+{
+
+    [SerializeField] int max_guess;
+    [SerializeField] int min_guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextGuess();
+
+    }
+
+
+    public void PressHigher()
+    {
+        if(min_guess<1000)
+        {
+            min_guess = guess + 1;
+            NextGuess();
+        }
+    }
+
+    public void PressLower()
+    {
+        max_guess = guess - 1;
+        NextGuess();
+
+    }
+
+
+
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min_guess, max_guess );
+        guessText.text = guess.ToString();
+
+    }
+
+}
